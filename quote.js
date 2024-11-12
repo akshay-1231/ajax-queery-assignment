3ce0611ae12cf36b154306602774c26a95e1dc4e
akshay-1231 <akshayprajapati424206@gmail.com>
2024-11-13 00:11:36 +0530

    Add files via upload

--- quote.js
+++ quote.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+document.getElementById("btn").addEventListener("click", function () {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        console.log("ReadyState:", xmlhttp.readyState);
+        console.log("Status:", xmlhttp.status);
+        
+        if (xmlhttp.readyState == 4 && xmlhttp.status ==     200) {
+            var res = JSON.parse(xmlhttp.responseText);
+
+            if (res) {
+                var type = res.type;
+                var quote = res.quote;
+                document.getElementById("quotes").innerHTML = `
+                <p>Quote - ${quote}</p>
+                <p>Type - ${type}</p>
+               `;
+            } else {
+                document.getElementById("quotes").innerHTML = `<p>No Quotes are available</p>`;
+            }
+        } else if (xmlhttp.readyState === 4) {
+            document.getElementById("quotes").innerHTML = `<p>Unable to fetch data</p>`;
+        }
+    };
+
+    xmlhttp.open("GET", "", true);
+    xmlhttp.send();
+});
+       
